5476600600ecc4eb40b3f33268366556325757ce
Simon Traels Ravn <cs2@cs2.dk>
2013-12-17 10:17:25 +0000

    Fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 UserImpersonate::Engine.routes.draw do
   get "/user/:user_id" , to: "impersonate#create", as: :impersonate_user
+  delete "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
   post "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
   
   root to: "impersonate#index"
