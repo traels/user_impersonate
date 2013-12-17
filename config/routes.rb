e0bd50d1cca14649bb312ecc420e800bf7f0e827
Simon Traels Ravn <cs2@cs2.dk>
2013-12-17 09:21:26 +0000

    Fixed route error on reverting from imporsonation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 UserImpersonate::Engine.routes.draw do
   get "/user/:user_id" , to: "impersonate#create", as: :impersonate_user
-  get "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
+  post "/revert" , to: "impersonate#destroy", as: :revert_impersonate_user
   
   root to: "impersonate#index"
 end
